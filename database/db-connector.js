a29f0b10599c87f262ff49e11d05437b8bf3501f
Christopher Nguyen <ctopherngoc@gmail.com>
2023-11-13 13:38:49 -0800

    Updated database information

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'databases.000webhost.com',
-  user            : 'id21133066_admin1',
-  password        : 'Password123!',
-  database        : 'id21133066_pokemon_db',
+  host            : 'db4free.net',
+  user            : 'ctopherngoc@localhost',
+  password        : '',
+  database        : 'pokemon_database',
   dateStrings: true
 });
 module.exports.pool = pool;
